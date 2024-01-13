8700d17e81dfa97a268bb7e229a0e4fb873978a0
Chris <138630942+CrisCo116@users.noreply.github.com>
2024-01-13 13:48:33 -0800

    fixed syntax

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,56 +1,73 @@
-const {User, Book} = require('../models');
-const {signToken, AuthenticationError} = require('../utils/auth');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if(context.user){
-                return User.findOne({_id: context.user._id}).populate('books');
-            }
-            throw new AuthenticationError('Not logged in');
-        },
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id });
+      }
+      return null;
     },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ $or: [{ username: email }, { email }] });
+      if (!user) {
+        throw new Error("Can't find this user");
+      }
 
-    Mutation: {
-        login: async (parent, {email, password}) => {
-            const user = await User.findOne({email});
-            if(!user){
-                throw new AuthenticationError('Incorrect credentials');
-            }
-            const correctPw = await user.isCorrectPassword(password);
-            if(!correctPw){
-                throw new AuthenticationError('Incorrect credentials');
-            }
-            const token = signToken(user);
-            return {token, user};
-        },
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-            return {token, user};
-        },
-        saveBook: async (parent, {bookData}, context) => {
-            if(context.user){
-                const updatedUser = await User.findByIdAndUpdate(
-                    {_id: context.user._id},
-                    {$push: {savedBooks: bookData}},
-                    {new: true}
-                );
-                return updatedUser;
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        },
-        removeBook: async (parent, {bookId}, context) => {
-            if(context.user){
-                const updatedUser = await User.findOneAndUpdate(
-                    {_id: context.user._id},
-                    {$pull: {savedBooks: {bookId: bookId}}},
-                    {new: true}
-                );
-                return updatedUser;
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        },
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new Error('Wrong password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
     },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      if (!user) {
+        throw new Error('Something is wrong!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { bookData }, context) => {
+      if (!context.user) {
+        throw new Error('Authentication required');
+      }
+
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookData } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      } catch (err) {
+        console.log(err);
+        throw new Error('Error saving book');
+      }
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (!context.user) {
+        throw new Error('Authentication required');
+      }
+
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId } } },
+        { new: true }
+      );
+
+      if (!updatedUser) {
+        throw new Error("Couldn't find user with this id!");
+      }
+      return updatedUser;
+    },
+  },
 };
-   module.exports = resolvers;
+
+module.exports = resolvers;
